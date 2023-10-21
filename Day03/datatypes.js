bf7b0138629707f95cc9615aedfd50f3bb293cab
Zain Ul Abdeen <77275855+zainhacer@users.noreply.github.com>
2023-10-21 04:35:36 +0000

    datatypes

---
+++ Day03/datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS as newer version/javascript
+
+// alert(3 + 3) //we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3) // code readibility should be high
+
+console.log("Zain")
+
+
+// DatatTypes
+let name = "Zain"
+let age = 20
+let isLoggedIn = false
+
+//number = 12
+//bigint =
+//string = ""
+//boolean = true/false
+//null = standlonee value
+//undefined = not assigned value
+//symbol = unique
+
+// Object
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
