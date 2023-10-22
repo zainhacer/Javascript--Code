2cb05e54dcb19329d8a7cbddf9ef19768903d208
Zain Ul Abdeen <77275855+zainhacer@users.noreply.github.com>
2023-10-22 03:45:40 +0000

    conversion of datatypes

---
+++ Day04/conversionOperations.js
@@ -0,0 +1,34 @@
+// Type conversion
+
+
+// let score = 33
+// let score = "33"
+// let score = "33abc"
+// let score = null
+let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "Zain"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "Zain" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
