c6ebc729d3b1211a81998d9ba5c52cd369a51749
Zain Ul Abdeen <77275855+zainhacer@users.noreply.github.com>
2023-10-22 04:34:07 +0000

    conversion of datatypes

--- Day04/conversionOperations.js
+++ Day04/conversionOperations.js
@@ -30,5 +30,51 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ******************************************************Operations**************************************************************
+
+
+let value = 5
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%4);
+
+
+let str1 = "Hello"
+let str2 = " Zain"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(true); //right method
+// console.log(+true);
+// console.log(+"");
+
+
+let gameCounter = 100
+++gameCounter;
+// console.log(gameCounter);
+
+
+let a = 1;
+console.log(++a); // 2
+console.log(a++); // 2
+console.log(a); // 3
