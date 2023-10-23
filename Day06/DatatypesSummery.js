55990c15fa960131c571545d4a93fefcfd2c4db4
Zain Ul Abdeen <77275855+zainhacer@users.noreply.github.com>
2023-10-23 04:33:54 +0000

    Summery Of Datatypes

---
+++ Day06/DatatypesSummery.js
@@ -0,0 +1,30 @@
+// Primitive Datatypes
+// 7 types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outSideTemp = null
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id === anotherId)
+
+// Reference (Non Premitive)
+// Array, Objects, Functions
+
+const names = ["Umair, Waheed, Uzair"];
+let myObj = {
+    name : "Zain",
+    age : 23,
+}
+
+const myFunction = function(){
+    console.log("hello world")
+
+}
+
+console.log(typeof myFunction)
