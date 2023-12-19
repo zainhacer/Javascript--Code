e196b521bf072220d4f9737f493a91169c983d6d
Zain Ul Abdeen <77275855+zainhacer@users.noreply.github.com>
2023-12-19 09:03:26 +0000

    Control flow in javascript in 1 sh

---
+++ Control_Flow/one.js
@@ -0,0 +1,63 @@
+// if
+
+// const isUserLoggedIn = true
+// const temprature = 41
+
+// if (temprature < 50){
+//     console.log("temprature less than 50");
+// } else{
+//     console.log("temprature is greater than 50");
+// }
+
+// if(2 === "2") {
+// console.log(excuted);
+// }
+
+// conditions statement
+// <, >, <=, >=, ==, !=, ===
+
+
+// const score = 200
+
+// if (score > 100) {
+//     power = 'fly'
+//     console.log(`User power: ${power}`);
+// }
+
+
+
+// short hand code - (not recommended)
+// const balance = 1000
+
+// if(balance > 500) console.log("test"), console.log(test2);
+
+
+// const balance = 1000
+
+// if(balance < 500){
+//     console.log("less than 500");
+// } else if(balance < 800){
+//     console.log("less than 800");
+// } else if(balance < 950){
+//     console.log("less than 950");
+// }
+// else{
+//     console.log('less than 1200');
+// }
+
+
+const isUserLoggedIn = true
+const debitCard = true
+const userAge = 18
+const loggedInEmail = true
+const loggedInGoogle = true
+
+// if(isUserLoggedIn && debitCard && 2==2 && userAge){
+//     console.log("Allow to buy course");
+// } else{
+//     console.log("Not allowed something missing");
+// }
+
+if (loggedInGoogle || loggedInEmail){
+    console.log("User logged In");
+}
