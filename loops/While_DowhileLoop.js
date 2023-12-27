a341df889d48162fdea363d967c8b7371df243fc
Zain Ul Abdeen <77275855+zainhacer@users.noreply.github.com>
2023-12-27 06:45:19 +0000

    while loops

---
+++ loops/While_DowhileLoop.js
@@ -0,0 +1,22 @@
+// While and do while loop in Javascript
+let i = 0
+// while (i <= 10) {
+//     console.log(`value of index is ${i}`);
+//     i = i + 2
+// }
+
+let myArray = ['AironMan', 'Batman', 'Mangoman', 'spiderman']
+let array = 0
+// while (array < myArray.length) {
+//     console.log(`Value is ${myArray[array]}`);
+//     array = array + 1
+// }
+
+
+
+// Do while loop
+let score = 1
+do {
+    console.log(`The score is ${score}`);
+    score++
+} while (score <= 10);
