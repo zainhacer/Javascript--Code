dc6811296af389b3fdbfbc15c4e5fa1b9daf58cc
Zain Ul Abdeen <77275855+zainhacer@users.noreply.github.com>
2023-12-29 05:07:42 +0000

    new

--- loops/forEach.js
+++ loops/forEach.js
@@ -13,3 +13,10 @@ const coding = ["js", "css", "html", "react", "ruby", "swift"]
 coding.forEach( (item, index, arr)=> {
     console.log(item, index, arr);
 } )
+
+
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const newNums = myNums.filter( (num) => num > 4)
+console.log(newNums);
